15f15faffe3385df65a95f54ef72dd442949c9d1
Erik Kristensen <erik@erikkristensen.com>
2014-12-08 12:36:32 -0700

    fixing container

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ if [ "$REGISTRY_HOSTNAME" = "" ] || [ "$INDEX_HOSTNAME" = "" ]; then
   exit 10
 fi
 
+sed -i "s/REGISTRY_IP/$REGISTRY_PORT_5000_TCP_ADDR/g" /etc/nginx/sites-enabled/default
+sed -i "s/INDEX_IP/$INDEX_PORT_5000_TCP_ADDR/g" /etc/nginx/sites-enabled/default
+
 sed -i "s/REGISTRY_HOSTNAME/$REGISTRY_HOSTNAME/g" /etc/nginx/sites-enabled/default
 sed -i "s/INDEX_HOSTNAME/$INDEX_HOSTNAME/g" /etc/nginx/sites-enabled/default
 
